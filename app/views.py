f6aafe72510e8d5264645d920b601950174d7676
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-03-26 16:51:09 -0500

    fixed form on_validate function

--- app/views.py
+++ app/views.py
@@ -7,11 +7,12 @@ from .forms import UrlForm
 def index():
 	form = UrlForm()
 	img_url = ''
+	tags_dict = {}
+
 	if form.validate_on_submit():
-		img_url = form.imageurl.data
-		return redirect('/')
+		img_url = str(form.imageurl.data)
+		tags_dict = get_tags(img_url)
 
-	img_url = 'http://vignette1.wikia.nocookie.net/joke-battles/images/4/40/18360-doge-doge-simple.jpg/revision/latest?cb=20151209161638'
-	tags_dict = get_tags(img_url)
-
+	# img_url = 'http://vignette1.wikia.nocookie.net/joke-battles/images/4/40/18360-doge-doge-simple.jpg/revision/latest?cb=20151209161638'
+	
 	return render_template('index.html', tags=tags_dict, img=img_url, form=form)
